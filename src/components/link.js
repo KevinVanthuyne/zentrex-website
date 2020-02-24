af44c72491c282926e81b051ab0060e3e4e9626e
Kevin Vanthuyne <kevin.vanthuyne2@gmail.Com>
2020-02-24 15:57:15 +0100

    added nicer styling to link component

--- src/components/link.js
+++ src/components/link.js
@@ -1,7 +1,14 @@
 import React from "react"
 
+import styles from "./link.module.scss"
+
 export default props => (
-  <a href={props.href} target="_blank" rel="noopener noreferrer">
+  <a
+    href={props.href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={styles.a}
+  >
     {props.children}
   </a>
 )
